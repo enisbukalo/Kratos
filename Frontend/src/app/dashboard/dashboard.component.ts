17bff92f4ef8280783b6a5a9ef0324f16fdff3b4
Enis Bukalo <bukaloenis@gmail.com>
2024-08-28 21:24:20 -0400

    Update
    - Working on styling a bit more.
    - Added workouts list on dashboard.

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { KratosServiceService } from '../kratos-service.service';
 import { CookieService } from 'ngx-cookie-service';
-import { UserReply } from '../kratos-api-types';
+import { UserReply, WorkoutReply } from '../kratos-api-types';
 import { ButtonModule } from 'primeng/button';
 import { AvatarModule } from 'primeng/avatar';
 import { SidebarModule } from 'primeng/sidebar';
@@ -9,11 +9,12 @@ import { PanelModule } from 'primeng/panel';
 import { DividerModule } from 'primeng/divider';
 import { Router } from '@angular/router';
 import { ChartModule } from 'primeng/chart';
+import { ScrollerModule } from 'primeng/scroller';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PanelModule, ChartModule, ButtonModule, AvatarModule, SidebarModule, PanelModule, DividerModule],
+  imports: [ScrollerModule, PanelModule, ChartModule, ButtonModule, AvatarModule, SidebarModule, PanelModule, DividerModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
@@ -22,28 +23,28 @@ export class DashboardComponent {
   router = inject(Router);
 
   currentUser: UserReply;
+  currentUsersWorkouts?: WorkoutReply[] = [];
   sidebarVisible: boolean = false;
   data: any;
   options: any;
 
   constructor(private apiService: KratosServiceService) {
     this.currentUser = JSON.parse(this.cookieService.get('currentUser'));
+    this.currentUsersWorkouts = this.currentUser.workouts;
     console.log("Current User: \n" + JSON.stringify(this.currentUser));
+
     this.data = {
       labels: ['January', 'February', 'March'],
       datasets: [
         {
           label: 'Test Weight',
-          data: [298, 297, 296],
+          data: [298, 296, 295],
         }
       ]
     };
+
     this.options = {
-      title: {
-        display: true,
-        text: 'Weight Chart',
-        fontSize: 16
-      },
+      responsive: true,
       legend: {
         position: 'bottom'
       }
