d1b6bd879061b0bcdb3a1a9383f7f953bd76632d
Enis Bukalo <bukaloenis@gmail.com>
2024-09-24 07:37:05 -0400

    Routing update.

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { WorkoutComponent } from './workout/workout.component';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
+    },
+    {
+        path: 'workout',
+        component: WorkoutComponent,
     }
 ];
